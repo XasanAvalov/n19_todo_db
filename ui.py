94e534405cadb67664f1df184f4caa097b9489b1
XasanAvalov <baxtiyarovic701@gmail.com>
2023-04-29 16:33:07 +0500

    Marking done

--- ui.py
+++ ui.py
@@ -12,9 +12,22 @@ class MainWindow (QWidget):
         self.db = Database()
 
         self.grid = QGridLayout()
+        self.render_all()
+        self.setLayout(self.grid)
     
     def render_all(self):
         data = self.db.get__all_tasks()
-        for row in data:
-            self.grid.addWidget()
 
+        c = [(x, 0, 1, 2) for x in range(len(data))]
+
+        for row, c in zip(data, c):
+            task = QLabel(str(row[0]) + ". " + str(row[1]))
+
+            if row[3] == 0:
+                task.setStyleSheet("font-weight: 700;")
+            else:
+                task.setStyleSheet("color: #333; font-size: 10px")
+
+            self.grid.addWidget(task, c[0], c[1])
+            chesk = QCheckBox()           
+            self.grid.addWidget(chesk, c[0], c[2])
